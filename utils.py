83247af1387d6a7c5a513644ca7aeab74812d881
khuevan <75727842+khuevan@users.noreply.github.com>
2022-07-19 16:27:07 +0700

    commit

--- utils.py
+++ utils.py
@@ -126,8 +126,6 @@ def crawl_data(driver, url):
     """
     if validators.url(url):
         driver.get(url)
-        time.sleep(2)
-
         images = driver.find_elements(By.TAG_NAME, 'img')
         div = driver.find_elements(By.TAG_NAME, 'div')
         html = driver.page_source
