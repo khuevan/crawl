0b5472a53099b58b6db2422d05a36e71554a12a4
khuevan <75727842+khuevan@users.noreply.github.com>
2022-07-19 11:51:19 +0700

    commit

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ load_dotenv(dotenv_path)
 
 HOST = os.getenv("HOST", "0.0.0.0")
 PORT = os.getenv("PORT", 8000)
-DEBUG = bool(os.environ.get("DEBUG", False))
+DEBUG = os.getenv("DEBUG", "False") == "True"
 CHROME_PATH = os.environ.get("CHROME_PATH")
 DOMAIN = os.environ.get("DOMAIN")
 HEIGHT = os.getenv("HEIGHT", 600)
